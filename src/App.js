f3d08e25bfab82baa8d51637d19842a5edbe7229
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-07-12 19:02:03 +0330

    bill gard has been created

--- src/App.js
+++ src/App.js
@@ -172,8 +172,14 @@ function FormSplitBill({ selectedFriend }) {
   const paidByFriend = bill ? bill - paidByUser : "";
   const [whoIsPaying, setWhoISPAying] = useState("user");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+  }
+
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>Split a bill with {selectedFriend.name} </h2>
 
       <label> Bill value </label>
